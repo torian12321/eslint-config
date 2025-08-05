154106c42a9da5b778065942f183cc19cfa38560
Aitor <aitorpalomares@hotmail.com>
2025-08-05 14:01:50 +0200

    ref: ignore examples folder

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import baseConfig from './src/base.js';
 
 export default [
-  { ignores: ['eslint.config.js'] },
+  { ignores: ['eslint.config.js', 'examples/**'] },
   ...baseConfig,
   {
     files: ['**/*.{js,ts,jsx,tsx}'],
